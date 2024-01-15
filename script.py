93fbf0a370d711440d74186ce4e3fee8c48ab2b6
Your Name <you@example.com>
2024-01-14 20:40:08 -0500

    update to water oxidation

--- script.py
+++ script.py
@@ -8,9 +8,9 @@ from math import ceil
 API_KEY = os.environ['OPENALEX_API_KEY']
 
 today = datetime.date.today()
-week_ago = (today - datetime.timedelta(weeks=6)).strftime("%Y-%m-%d")
+day_ago = (today - datetime.timedelta(days=7)).strftime("%Y-%m-%d")
 
-url = f'https://api.openalex.org/works?filter=author.id:https://openalex.org/A5003442464,from_created_date:{week_ago}&api_key={API_KEY}'
+url = f'https://api.openalex.org/works?filter=title_and_abstract.search:oxygen+evolution,from_created_date:{day_ago}&api_key={API_KEY}'
 
 data = requests.get(url).json()
 
@@ -20,14 +20,44 @@ npages = ceil(count / perpage)
 
 def process_result(result):
     authors = ', '.join([au['author']['display_name'] for au in result['authorships'] ])
+    word_index = []
+
+    aii = result.get('abstract_inverted_index', None)
+
+    if aii:
+        for k,v in aii.items():
+            for index in v:
+                word_index.append([k, index])
+
+        word_index = sorted(word_index,key = lambda x : x[1])
+        abstract = ' '.join([x[0] for x in word_index])
+    else:
+        abstract = 'No abstract'
+
+    source = result.get('primary_location', {}).get('source', {})
+    if source:
+        host = source.get('display_name', 'No host')
+    else:
+        host = 'No host'
+
     return f'''** {result['title']}
 :PROPERTIES:
 :ID: {result['id']}
 :DOI: {result['doi']}
 :AUTHORS: {authors}
-:HOST: {result['primary_location']['source']['display_name']}    
+:HOST: {host}
 :END:
 
+    
+[[elisp:(doi-add-bibtex-entry "{result['doi']}")][Get bibtex entry]]
+
+- [[elisp:(progn (xref--push-markers (current-buffer) (point)) (oa--referenced-works "{result['id']}"))][Get references]]
+- [[elisp:(progn (xref--push-markers (current-buffer) (point)) (oa--related-works "{result['id']}"))][Get related work]]
+- [[elisp:(progn (xref--push-markers (current-buffer) (point)) (oa--cited-by-works "{result['id']}"))][Get cited by]]
+
+{abstract}    
+
+    
 '''
 
 # Process page 1
@@ -42,7 +72,6 @@ for i in range(1, npages):
         s += process_result(result)
   
     
-with open('results.org', 'a') as f:
+with open('results.org', 'w') as f:
+    f.write(f'* Results for {day_ago}\n\n')
     f.write(s)
-
-print(s)
