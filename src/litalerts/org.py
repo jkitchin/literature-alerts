38c78a0b90180666a3bf7e663979bcb8af160d8a
John Kitchin <jkitchin@andrew.cmu.edu>
2024-01-17 12:04:03 -0500

    add number of results

--- src/litalerts/org.py
+++ src/litalerts/org.py
@@ -52,7 +52,7 @@ def write_org(topic, results):
         f.write(f'#+filetags: {topic["label"].replace(" ", "_")}\n')
         f.write(f'#+TITLE: {topic["label"]}\n')
         f.write(f'Created on {time.asctime()}\n\n')
-        f.write(f'Results from {topic["since"]} to {topic["today"]}\n')
+        f.write(f'Found {len(results)} results from {topic["since"]} to {topic["today"]}\n')
         f.write(s)
         
 
